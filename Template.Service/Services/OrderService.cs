494a70f470120cfbbed7fa8aac8831ef489037ac
Cyrus Zhou <cyrus.zhou@outlook.com>
2017-04-22 17:26:00 +0800

    继承构造函数

--- Template.Service/Services/OrderService.cs
+++ Template.Service/Services/OrderService.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template.Entity;
+using Template.Repository;
+using Template.Repository.IRepositories;
 using Template.Service.IServices;
 
 namespace Template.Service.Services
 {
     public class OrderService : Service<Orders>, IOrderService
     {
+        private readonly IOrderRepository _orderRepository;
+        public OrderService(IOrderRepository orderRepository) : base(orderRepository)
+        {
+            this._orderRepository = orderRepository;
+        }
     }
 }
--- Template.Service/Services/ProductService.cs
+++ Template.Service/Services/ProductService.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template.Entity;
+using Template.Repository;
+using Template.Repository.IRepositories;
 using Template.Service.IServices;
 
 namespace Template.Service.Services
 {
     public class ProductService : Service<Products>, IProductService
     {
+        private readonly IProductRepository _productRepository;
+        public ProductService(IProductRepository productRepository) : base(productRepository)
+        {
+            this._productRepository = productRepository;
+        }
     }
 }
